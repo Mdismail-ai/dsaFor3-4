b97f0e56a3a57eb1ea3ec90fe22dc8e10a14fa8a
Mdismail-ai <mdismail99059331075@gmail.com>
2026-06-22 11:06:37 +0530

    4thcommit

--- bestDaytoBuyAndSellStocks.java
+++ bestDaytoBuyAndSellStocks.java
@@ -11,6 +11,7 @@ public class bestDaytoBuyAndSellStocks {
         return maxProfit;
     }
     public static void main(String[] args) {
-        
+        int nums[] = {7,1,5,3,6,4};
+       System.out.println(bestDaytoBuyAndSellStockss(nums));
     }
 }
